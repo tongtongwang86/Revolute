1b75098cc3995badaabeb6a2a4013768957150f2
Tong tong <t0ngx2@icloud.com>
2023-12-27 16:45:28 +0800

    added revolute wireless(work in progress)

---
+++ Software/Sourcev2/boards/arm/divisionL/board.cmake
@@ -0,0 +1,7 @@
+
+
+
+board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
+
+include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
